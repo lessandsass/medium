d0cfc478acc3e61a4bcc7d5b3263017ada918720
Less <31947204+lessandsass@users.noreply.github.com>
2020-11-07 06:52:00 +0530

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $posts = Post::with('user')->latest()->paginate(20);
+
+        return view('posts.index', [
+            'posts' => $posts,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required|max:200',
+        ]);
+ 
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
+    }
+
+    public function destroy(Request $request, Post $post)
+    {
+
+        $this->authorize('delete', $post);
+
+        $post->delete();
+
+        return back();
+    }
+
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Post;
+use App\Policies\PostPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Post::class => PostPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        // Gate::define('delete-post', function (User $user, Post $post) {
+        //     return $user->id === $post->user_id;
+        // });
+    }
+}
